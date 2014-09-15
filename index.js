d084562071083e8eb9e542de0d8e03ee600bf59c
Brian M Hunt <brianmhunt@gmail.com>
2014-09-15 16:24:31 -0400

    More open/close events

--- index.js
+++ index.js
@@ -16,17 +16,28 @@
   var $html = $("html");
   var TRANSITION_END = "webkitTransitionEnd oTransitionEnd otransitionend " +
     "transitionend msTransitionEnd";
-  var open_count = 0;
+  var open_modals = ko.observableArray();
 
   function apply_backdrop() {
     $html.addClass('kom-active');
   }
 
   function remove_backdrop() {
-    if (open_count == 0) {
+    if (open_modals().length == 0) {
       $html.removeClass('kom-active');
     }
   }
+
+  $(document.body).on('click', '.kom-modal', function (evt) {
+    var class_list = evt.target.classList;
+    if (class_list.contains("kom-modal") || class_list.contains("kom-dialog")) {
+      modals = open_modals();
+      if (modals.length == 0) {
+        return
+      }
+      modals[modals.length - 1].deactivate()
+    }
+  });
 
   var KnockoutModal = (function() {
     var KM = function KnockoutModal(template, data, options) {
@@ -44,28 +55,35 @@
 
       this.display = ko.observable();
       this.display.subscribe(this.on_display_change, this);
-      this.display(this.options.show != false)
+      this.display(this.options.show != false);
     }
 
     KM.prototype.activate = function () {
-      open_count++;
+      open_modals.push(this);
       apply_backdrop();
     };
 
     KM.prototype.deactivate = function () {
       var node = this.$node;
-      open_count--;
+      var callback = this.options.afterClose;
+      open_modals.remove(this);
       remove_backdrop();
       function on_hide() {
-        node.remove()
+        node.remove();
+        if (typeof callback == 'function') {
+          callback();
+        }
       }
-
       node.addClass('kom-remove-animation').one(TRANSITION_END, on_hide);
     };
 
     KM.prototype.on_display_change = function (display) {
       display ? this.activate() : this.deactivate();
     };
+
+    KM.prototype.index = function () {
+      return open_modals.indexOf(this);
+    }
 
     function on_close_click() {
       this.deactivate();
--- spec/test.js
+++ spec/test.js
@@ -5,6 +5,40 @@
 
 var nest_count = 0;
 
+var ipsum = (
+  "Seamlessly provide   access to scalable e-tailers for interoperable" +
+  "web-readiness. Uniquely   customize economically sound initiatives" +
+  "vis-a-vis sustainable models.   Interactively enhance technically sound" +
+  "bandwidth through resource maximizing   users. Dynamically reinvent" +
+  "turnkey vortals whereas interactive partnerships.   Quickly target 24/7" +
+  "paradigms via worldwide leadership.\n" +
+
+  "Globally matrix team driven interfaces without collaborative scenarios." +
+  "Continually transform an expanded array of data without end-to-end ROI." +
+  "Professionally transition economically sound communities vis-a-vis just" +
+  "in time value. Competently develop inexpensive potentialities after" +
+  "reliable e-tailers. Authoritatively procrastinate clicks-and-mortar" +
+  "markets through standards compliant catalysts for change.\n" +
+
+  "Globally matrix team driven interfaces without collaborative scenarios." +
+  "Continually transform an expanded array of data without end-to-end ROI." +
+  "Professionally transition economically sound communities vis-a-vis just" +
+  "in time value. Competently develop inexpensive potentialities after" +
+  "reliable e-tailers. Authoritatively procrastinate clicks-and-mortar" +
+  "markets through standards compliant catalysts for change.\n" +
+
+  "Globally matrix team driven interfaces without collaborative scenarios." +
+  "Continually transform an expanded array of data without end-to-end ROI." +
+  "Professionally transition economically sound communities vis-a-vis just" +
+  "in time value. Competently develop inexpensive potentialities after" +
+  "reliable e-tailers. Authoritatively procrastinate clicks-and-mortar" +
+  "markets through standards compliant catalysts for change.\n" +
+
+  "Collaboratively network resource sucking e-commerce rather than" +
+  "e-business mindshare. Dynamically impact turnkey e-business via" +
+  "value-added."
+).split('\n');
+
 var view_model = {
   open_basic_click: function () {
     var data = {
@@ -14,40 +48,7 @@ var view_model = {
   },
 
   open_scroll_click: function () {
-    var data = {
-      ipsum:
-        "Seamlessly provide   access to scalable e-tailers for interoperable" +
-        "web-readiness. Uniquely   customize economically sound initiatives" +
-        "vis-a-vis sustainable models.   Interactively enhance technically sound" +
-        "bandwidth through resource maximizing   users. Dynamically reinvent" +
-        "turnkey vortals whereas interactive partnerships.   Quickly target 24/7" +
-        "paradigms via worldwide leadership." +
-
-        "Globally matrix team driven interfaces without collaborative scenarios." +
-        "Continually transform an expanded array of data without end-to-end ROI." +
-        "Professionally transition economically sound communities vis-a-vis just" +
-        "in time value. Competently develop inexpensive potentialities after" +
-        "reliable e-tailers. Authoritatively procrastinate clicks-and-mortar" +
-        "markets through standards compliant catalysts for change." +
-
-        "Globally matrix team driven interfaces without collaborative scenarios." +
-        "Continually transform an expanded array of data without end-to-end ROI." +
-        "Professionally transition economically sound communities vis-a-vis just" +
-        "in time value. Competently develop inexpensive potentialities after" +
-        "reliable e-tailers. Authoritatively procrastinate clicks-and-mortar" +
-        "markets through standards compliant catalysts for change." +
-
-        "Globally matrix team driven interfaces without collaborative scenarios." +
-        "Continually transform an expanded array of data without end-to-end ROI." +
-        "Professionally transition economically sound communities vis-a-vis just" +
-        "in time value. Competently develop inexpensive potentialities after" +
-        "reliable e-tailers. Authoritatively procrastinate clicks-and-mortar" +
-        "markets through standards compliant catalysts for change." +
-
-        "Collaboratively network resource sucking e-commerce rather than" +
-        "e-business mindshare. Dynamically impact turnkey e-business via" +
-        "value-added."
-    };
+    var data = { ipsum: ipsum };
     new KnockoutModal('test-scroll', data, {show: true});
   },
 
@@ -56,8 +57,41 @@ var view_model = {
       count: nest_count++,
       open_click: view_model.open_nested_click,
     };
-    new KnockoutModal('test-nested', data, {show: true});
+    new KnockoutModal('test-nested', data, {
+      show: true,
+      afterClose: function () { nest_count-- },
+    });
   },
+
+  open_history_click: function () {
+    var data = {
+      count: nest_count++,
+      open_click: view_model.open_history_click,
+    };
+
+    var kom = new KnockoutModal('test-nested', data, {
+      show: true,
+      afterClose: function () {
+        history.back()
+      }
+    });
+
+    var state = {
+      idx: kom.index(),
+      data: data,
+      template: 'test-nested',
+    };
+
+    history.pushState(state);
+  }
 };
+
+
+$(window).on('popstate', function () {
+  if (!history.state) {
+    return;
+  }
+  console.log("STATE", history.state);
+});
 
 ko.applyBindings(view_model);
